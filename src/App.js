df4da3385902706d7db47ce3114149a88e6c0887
Tri Börman <wittyangel_98@hotmail.com>
2022-10-10 10:45:37 +0300

    Img

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Header from "./components/Header";
 import Basket from "./components/Basket";
 import Main from "./components/Main";
+import Img from "./components/Img";
+
 function App() {
   return (
     <div>
       <Header />
       <div className="row">
+        <img />
         <Main />
         <Basket />
       </div>
+      <Img />
     </div>
   );
 }
