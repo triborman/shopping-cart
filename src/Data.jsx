cf3bc8b76da0f5e0f557404bd3fd6d0a01c235cb
Tri Börman <wittyangel_98@hotmail.com>
2022-10-12 09:56:56 +0300

    modified

---
+++ src/Data.jsx
@@ -0,0 +1,16 @@
+const data = {
+  products: [
+    {
+      id: "1",
+      name: "Macbook",
+      price: 1400,
+      image: "https://picsum.photos/id/180/2400/1600",
+    },
+    {
+      id: "2",
+      name: "Old Car",
+      price: 2400,
+      image: "https://picsum.photos/id/111/4400/2656",
+    },
+  ],
+};
