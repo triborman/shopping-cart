642b140576a6ff3da89049c36890958bf876bcc5
Tri Börman <wittyangel_98@hotmail.com>
2022-10-25 11:28:32 +0300

    deploy

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,19 @@
+import Product from "./Product";
+
 export default function Main(props) {
-  const { products } = props;
+  const { cartItems, products, onAdd, onRemove } = props;
   return (
     <div className="block col-2">
       <h2>Products</h2>
       <div className="row">
         {products.map((product) => (
-          <div key={product.id}>{product.name}</div>
+          <Product
+            key={product.id}
+            product={product}
+            item={cartItems.find((x) => x.id === product.id)}
+            onAdd={onAdd}
+            onRemove={onRemove}
+          ></Product>
         ))}
       </div>
     </div>
